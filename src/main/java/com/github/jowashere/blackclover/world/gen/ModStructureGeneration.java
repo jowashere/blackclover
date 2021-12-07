692fb64a1c8611d4d189e1c6b766f400eb30a315
Beosti <44171315+Ziroxis@users.noreply.github.com>
2021-12-07 17:23:56 +0100

    Additions: >System to check if a structure spawns in a specific type of biome
    Changes:
    >The magic tower now only spawns in plain biomes
    >Made it way less common for the structure to generate

--- src/main/java/com/github/jowashere/blackclover/world/gen/ModStructureGeneration.java
+++ src/main/java/com/github/jowashere/blackclover/world/gen/ModStructureGeneration.java
@@ -1,23 +1,24 @@
 package com.github.jowashere.blackclover.world.gen;
 
-import com.github.jowashere.blackclover.init.StructuresInit;
 import com.github.jowashere.blackclover.world.structure.configured.ConfiguredStructures;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.feature.IFeatureConfig;
-import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 
-import java.util.List;
 import java.util.Set;
-import java.util.function.Supplier;
+
 
 public class ModStructureGeneration
 {
     public static void generateStructures(final BiomeLoadingEvent event)
     {
-            event.getGeneration().getStructures().add(() -> ConfiguredStructures.CONFIGURED_MAGICTOWER); //TODO make it generate in specific biomes and villages
+        RegistryKey key = RegistryKey.create(Registry.BIOME_REGISTRY, event.getName());
+        Set types = BiomeDictionary.getTypes(key);
+
+        if (types.contains(BiomeDictionary.Type.PLAINS))
+        {
+            event.getGeneration().getStructures().add(() -> ConfiguredStructures.CONFIGURED_MAGICTOWER);
+        }
     }
 }
