f410208d44626125ab7be00257e5f0ef834dc427
Beosti <44171315+Ziroxis@users.noreply.github.com>
2021-12-07 17:23:56 +0100

    Additions: >System to check if a structure spawns in a specific type of biome
    Changes:
    >The magic tower now only spawns in plain biomes
    >Made it way less common for the structure to generate

--- src/main/java/com/github/jowashere/blackclover/world/structure/structures/MagicTowerStructure.java
+++ src/main/java/com/github/jowashere/blackclover/world/structure/structures/MagicTowerStructure.java
@@ -24,6 +24,7 @@ import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.jigsaw.JigsawManager;
 import net.minecraft.world.gen.feature.structure.*;
 import net.minecraft.world.gen.feature.template.TemplateManager;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 
