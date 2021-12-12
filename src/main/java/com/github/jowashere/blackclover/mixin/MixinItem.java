a517f4606755665dae64106d433f60c03cb7ed23
jowashere <54336182+jowashere@users.noreply.github.com>
2021-12-12 18:07:11 +0000

    Issues that caused crash fixed.

--- src/main/java/com/github/jowashere/blackclover/mixin/MixinItem.java
+++ src/main/java/com/github/jowashere/blackclover/mixin/MixinItem.java
@@ -5,7 +5,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Item.class)
 public abstract class MixinItem {
 
-    @Shadow public abstract void inventoryTick(ItemStack pStack, World pLevel, Entity pEntity, int pItemSlot, boolean pIsSelected);
+    //@Shadow public abstract void inventoryTick(ItemStack pStack, World pLevel, Entity pEntity, int pItemSlot, boolean pIsSelected);
 
     @Inject(at = @At("HEAD"), method = "inventoryTick", cancellable = true)
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected, CallbackInfo ci) {
